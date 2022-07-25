2a11a0e2e40324f3ab308616efa363d1fdb55332
PedroMPagani <pedro.paulo.pagani@gmail.com>
2022-07-25 08:16:43 -0300

    Implement AccessType on class

--- src/main/java/org/unbbrasilia/fga0158/component/Access.java
+++ src/main/java/org/unbbrasilia/fga0158/component/Access.java
@@ -8,11 +8,13 @@ public class Access {
     private final String carPlate;
     private final Long enterMillis;
     private final Long leaveMillis;
+    private final AccessType accessType;
 
-    public Access(Long enter, String carPlate, Long leaveMillis){
+    public Access(Long enter, String carPlate, Long leaveMillis, AccessType type){
         this.carPlate = carPlate;
         this.enterMillis = enter;
         this.leaveMillis = leaveMillis;
+        this.accessType = type;
     }
 
 }
